88d1f72627b86f935d304971190b41c0cb438352
MaykonFranchini <maykonfranchini2017@gmail.com>
2022-06-23 04:25:59 +0100

    fetch news logic completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,37 @@
 import { useEffect, useState } from 'react'
 
 import { Header } from "./components/Header"
-import { Sidebar } from "./components/Sidebar"
+import { Club, Sidebar } from "./components/Sidebar"
 import './global.css'
 import  './App.module.css'
 import { NewsBox } from "./components/NewsBox"
 import { ClubProfile } from './components/ClubProfile'
-import { Club } from './components/Sidebar'
 
-import { clubs } from './clubsList'
 function App() {
 
-  // const [clubList, setClubList] = useState([]);
+  const [clubList, setClubList] = useState<Club[]>([]);
   const [selectedClub, setSelectedClub] = useState('');
-  const [selectedClubProfile, setSelectedClubProfile] = useState({ name: 'Flamengo', logo_url: "https://s.glbimg.com/es/sde/f/organizacoes/2018/04/10/Flamengo-2018.svg"});
+  const [selectedClubProfile, setSelectedClubProfile] = useState({ name: 'Flamengo', logo_url: "https://s.glbimg.com/es/sde/f/organizacoes/2018/04/10/Flamengo-2018.svg", source_url:"https://ge.globo.com/futebol/times/flamengo/"});
 
-  // useEffect(() => {
-  //  const getClubs = async() => {
-  //    const response = await fetch('http://localhost:3333/clubs')
-  //    const data = await response.json()
-  //    console.log(data)
-  //    setClubList(data)
-  //  }
+  useEffect(() => {
+   const getClubs = async() => {
+    const response = await fetch('https://football-news-api-production.up.railway.app/clubs')
+     const data = await response.json()
+     
+     setClubList(data.clubs)
+   }
 
-  //  getClubs()
-  // }, [])
-
-
+   getClubs()
+  }, [])
 
   function updateSelectedClub(club: string) {
     setSelectedClub(club)
 
-    const clubProfile = clubs.filter(team => team.name === club)
-
-
+    const clubProfile = clubList.filter(team => team.name === club)
+    
     setSelectedClubProfile({name: clubProfile[0].name,
-      logo_url: clubProfile[0].logo_url
+      logo_url: clubProfile[0].logo_url,
+      source_url: clubProfile[0].source_url
     })
 
   }
@@ -44,8 +40,8 @@ function App() {
     <>
       <Header/>
       <main>
-        {!selectedClub ? <Sidebar clubsList={clubs} club={selectedClub} onUpdateSelectedClub={updateSelectedClub} /> : <ClubProfile logo_url={selectedClubProfile.logo_url} name={selectedClubProfile.name} />}
-        <NewsBox club={selectedClub}/>
+        {!selectedClub ? <Sidebar clubsList={clubList} club={selectedClub} onUpdateSelectedClub={updateSelectedClub} /> : <ClubProfile logo_url={selectedClubProfile.logo_url} name={selectedClubProfile.name} />}
+        <NewsBox club={selectedClub} />
 
       </main>
     </>
--- src/components/NewsBox.tsx
+++ src/components/NewsBox.tsx
@@ -1,12 +1,39 @@
 import styles from './NewsBox.module.css'
+import { Divide, Newspaper, SoccerBall, SpeakerNone } from 'phosphor-react'
+import { useEffect, useState } from 'react'
 
-import { Newspaper } from 'phosphor-react'
+interface News {
+  title: string;
+  link: string;
+  img_url: string;
+}
+interface NewsBoxProps {
+  club?: string
+}
 
-export function NewsBox({club}:any) {
+export function NewsBox({ club }:NewsBoxProps) {
+  const [news, setNews] = useState<News[]>([]);
+  
+
+  useEffect(()=> {
+    
+    const url = `https://football-news-api-production.up.railway.app/latestsnews/${club}`
+    const getNews = async() => {
+      const response = await fetch(url)
+      const {data} = await response.json()
+            
+      setNews(data)
+    }
+  
+    getNews() 
+  },[club])
+
+
+  console.log(news);
   
   return (
 
-        <div>
+    <div>
         {!club &&  
           <div className={styles.newsBox}>
             <Newspaper size={48} />
@@ -14,7 +41,21 @@ export function NewsBox({club}:any) {
           </div>
         }
 
-        <div>{club}</div>
+        <div className={styles.newsWrapper}>
+          {news.length <= 0 && club && <div className={styles.loading}><span ><SoccerBall size={32} /><strong>{club}</strong><SoccerBall size={32} /></span></div>}
+          
+            {news.map(article => {
+                return (
+                  <div className={styles.newsCard} key={article.title}>
+                     <img src={article.img_url} />
+                     <a href={article.link}><p>{article.title}</p></a>
+                  </div>
+                )
+              }
+             )
+            }
+          
         </div>
+    </div>
   )
 }
