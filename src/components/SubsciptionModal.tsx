050e5803fa7e0ef2bd01b032e9e72143f6b89a45
MaykonFranchini <maykonfranchini2017@gmail.com>
2022-07-04 06:33:53 +0100

    modal form  styled

--- src/components/SubsciptionModal.tsx
+++ src/components/SubsciptionModal.tsx
@@ -1,5 +1,8 @@
 import Modal from 'react-modal';
 import { useForm, SubmitHandler } from "react-hook-form";
+import styles from './SubscriptionModal.module.css';
+import { X } from 'phosphor-react';
+
 
 interface SubscriptioModalProps {
   isOpen: boolean;
@@ -19,18 +22,23 @@ export function SubscriptioModal({isOpen, onRequestClose, onSubmit}: Subscriptio
   const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
 
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Receba as ultimas noticias do seu clube por email</h2>
-      <button onClick={onRequestClose}>X</button>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" placeholder="Digite seu email" {...register('email', {required: true})} />
-        {errors.email?.type === 'required' && "Email é obrigatório"}
-        <input type="text" placeholder="Nome" {...register('first_name', {required: true})} />
-        {errors.first_name?.type === 'required' && "Nome é obrigatório"}
-        <input type="text" placeholder="Sobrenome" {...register('last_name', {required: true})} />
-        {errors.last_name?.type === 'required' && "Sobrenome é obrigatório"}
-        <input type="submit" value="Enviar" />
-      </form> 
+    <Modal isOpen={isOpen} onRequestClose={onRequestClose} overlayClassName='react-modal-overlay' className='react-modal-content' >
+      <div className={styles.modalWrapper}>
+        <h2>Receba as ultimas noticias do seu clube por email</h2>
+        <button onClick={onRequestClose} type='button'><X size={20} color="#1e3a8a" weight="bold" /></button>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {errors.email?.type === 'required' && <p className={styles.errorMsg}>Email é obrigatório</p>}
+          <input type="text" placeholder="Digite seu email" {...register('email', {required: true})} />
+          
+          {errors.first_name?.type === 'required' && <p className={styles.errorMsg}>Nome é obrigatório</p>}
+          <input type="text" placeholder="Nome" {...register('first_name', {required: true})} />
+          
+          {errors.last_name?.type === 'required' && <p className={styles.errorMsg}>Sobrenome é obrigatório</p>}
+          <input type="text" placeholder="Sobrenome" {...register('last_name', {required: true})} />
+          <input type="submit" value="Enviar" />
+        </form> 
+      </div>
     </Modal>
   )
 }
