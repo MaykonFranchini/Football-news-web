388a34f08e6f3e07ecde5cc0acef5f191cc71bbb
MaykonFranchini <maykonfranchini2017@gmail.com>
2023-05-12 12:03:51 +0100

    Feature: PWA config with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import { manifest } from './src/manifest-vite.js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: 'autoUpdate',
-    devOptions: {
-      enabled: true
-    }
-  })]
+  plugins: [react(), VitePWA(manifest)]
 })
